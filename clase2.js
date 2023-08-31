45016b7ba82361dd91b0194d4ad421c71359c0b6
clari111 <136344430+clari111@users.noreply.github.com>
2023-08-31 19:20:06 -0300

    Tarea que nos dejo el profe 6 problemas
    Tarea

--- clase2.js
+++ clase2.js
@@ -52,3 +52,63 @@ function Saludar (nombre, apellido)
 return  'bienvenido' + nombre + apellido
 }
 alert(Saludar(clara, gauna))
+
+function cubo (a)
+{ 
+    let calculaCubo = a * a * a
+return 'resultado del cubo' + calculaCubo
+} 
+alert(cubo(5))
+
+function cuadrado (b)
+{
+    let calcularCuadrado = b * b
+return 'el resultado del cuadrado es' + calcularCuadrado
+}
+alert(cuadrado(7))
+
+function calcularPotencia1 (x, a)
+{ 
+    let resultado = (x * x) + a
+
+    return resultado
+}
+alert (calcularPotencia1(5, 8)
+
+function calcularProblema2 (x, a, b)
+    
+{ let resultado = (x - a) * b
+ return 'Resultado del problema 2' + resultado
+}
+alert (calcularProblema2(4, 3, 2)
+
+    function calcularProblema3 ( x, a)
+{
+    let resultado = (x * x) * x * x + a
+    return 'El resultado del problema 3 es ' + resultado
+}
+alert (calcularProblema3 (6, 20)
+
+       
+function calcularProblema4 (x, a, b)
+{ 
+    let resultado = (x / a) + b
+    return 'El resultado del problema 4 es ' + resultado
+}
+alert (calcularProblema4(3, 2, 5)
+
+       function calcularProblema5 (x, a)
+{
+    let resultado = (x * x * x) + (x * x) + a
+    return 'El resultado del problema 5 es ' + resultado
+}
+alert (calcularProblema5( 3, 10)
+
+    function calcularProblema6 ( x, a, b)
+{
+    let resultado = (x * a) * b
+    return 'el resultado del problema 6 es ' + resultado
+}
+alert (calcularProblema6( 7, 10, 5)
+
+ 
